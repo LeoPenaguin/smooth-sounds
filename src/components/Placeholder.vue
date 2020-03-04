955983230216ebf720a2184c448064418ca0353f
Leo <penaguinleo@gmail.com>
2020-03-04 21:27:48 +0100

    First real commit

---
+++ src/components/Placeholder.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="Placeholder">
+    Placeholder
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Placeholder',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
